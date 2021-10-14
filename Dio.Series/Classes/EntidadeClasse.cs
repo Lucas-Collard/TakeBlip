740ec8ab88bf3d4eb1f899dd15c7f9161d754b15
Lucas Collard <lucas.collard@2rpnet.com>
2021-10-14 15:48:04 -0300

    Adicionar arquivos de projeto.

---
+++ Dio.Series/Classes/EntidadeClasse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio.Series.Classes
+{
+    class EntidadeClasse
+    {
+        public int id { get; protected set; }
+    }
+}
